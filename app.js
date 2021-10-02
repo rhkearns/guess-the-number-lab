917f8f22a5696ed94fce1b412fc1b7f38be2630e
rhkearns <ryankearns217@gmail.com>
2021-10-01 20:07:47 -0400

    create method getGuess in prompt

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
       return this.secretNum
+  },
+  getGuess: function(){
+    window.prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}`)
+
   }
 }
 
 console.log(game.play())
+game.getGuess()
