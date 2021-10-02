5dd5198a6f120052aab60a109c35e609cb303ac1
rhkearns <ryankearns217@gmail.com>
2021-10-02 11:24:52 -0400

    getGuess works

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const game = {
         window.prompt(`Lower! Previous guesses: ${this.prevGuesses}`)
       } else if (this.guess > this.smallestNum){
         window.prompt(`Higher! Previous guesses: ${this.prevGuesses}`)
-      } else 
+      } else {
+        window.prompt(`Correct! The secret number was ${this.secretNum}!`)
+      }
       prevGuesses.push(this.guess)
     } while (this.guess !== this.secretNum)
   },
@@ -24,13 +26,13 @@ const game = {
   getGuess() {
     let guessTest = window.prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}.`)
     guessTest = Number(guessTest)
-    if (guessTest < game.smallestNum || guessTest > game.biggestNum){
+    while (guessTest <= game.smallestNum || guessTest >= game.biggestNum){
       guessTest = window.prompt(`Try again: Enter a guess BETWEEN ${game.smallestNum} and ${game.biggestNum}`)
       guessTest = Number(guessTest)
-    } else {
+    } 
     this.guess = guessTest
-    }
     console.log(this.guess)
+    return this.guess
     },
 
 
@@ -39,4 +41,4 @@ const game = {
   }
 }
 
-game.getGuess()
+game.play()
