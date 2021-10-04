ec5c51baadbc7f9139be56469a87d0e51953eeb8
rhkearns <ryankearns217@gmail.com>
2021-10-04 11:11:32 -0400

    fix letter error and troubleshoot

--- app.js
+++ app.js
@@ -8,45 +8,43 @@ const game = {
   result: '',
   play() {
     this.secretNum = (Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum)
-    game.getGuess()
-    this.prevGuesses.push(this.guess)  
+      (this.biggestNum - this.smallestNum +1)) + this.smallestNum)
+
     do {
-      if (this.guess > this.secretNum) {
-        this.result = 'High'
-        this.render('incorrect')
-      } else if (this.guess < this.secretNum){
-        this.result = 'Low'
-        this.render('incorrect')
-      } else {
-        this.result = 'correct'
-        this.render('correct')
-        break;
-      }
-      this.prevGuesses.push(this.guess)
-    } while (this.guess !== this.secretNum)
-    return 
+      //game.getGuess()
+      this.prevGuesses.push(this.getGuess())
+      this.render()
+      } while (this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum)
+    //return 
   },
   
   
   getGuess() {
-    let guessTest = window.prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}.`)
-    guessTest = Number(guessTest)
-    while (guessTest <= game.smallestNum || guessTest >= game.biggestNum){
-      guessTest = window.prompt(`Try again: Enter a guess BETWEEN ${game.smallestNum} and ${game.biggestNum}`)
-      guessTest = Number(guessTest)
+    let guess 
+    
+    while (guess < game.smallestNum || guess > game.biggestNum || isNaN(guess)){
+      guess = parseInt(prompt(`Enter a guess, between ${game.smallestNum} and ${game.biggestNum}`))
     } 
-    this.guess = guessTest
-    return this.guess
+    return guess
+    //return this.guess = guess
     },
 
 
-  render(str){
-    if (str === 'incorrect')
-    this.guess = window.prompt(`Your guess is too ${this.result}. Previous guesses: ${this.prevGuesses}`)
-    else if (str === 'correct') {
-    window.alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guess(es)!`)
-    }
+  render(){
+    if ((this.prevGuesses[this.prevGuesses.length -1] === this.secretNum)) {
+    alert (`Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`)
+    } 
+    
+    else {
+      if (this.prevGuesses[this.prevGuesses.length -1] > this.secretNum) {
+    alert (`Your guess is too high! Previous guesses: ${this.prevGuesses}`)
+  } 
+  
+    else if (this.prevGuesses[this.prevGuesses.length -1] < this.secretNum) {
+    alert (`Your guess is too low. Previous guesses: ${this.prevGuesses}`)
+  }
+}  
+  
   }
 }
 
